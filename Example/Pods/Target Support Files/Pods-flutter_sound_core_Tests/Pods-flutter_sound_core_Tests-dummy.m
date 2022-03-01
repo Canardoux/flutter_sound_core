d67a5326465f783d9fa48d5c824f086639b21a7e
Larpoux <larpoux@gmail.com>
2022-03-01 20:34:52 +0100

    TAU : Version 9.1.4

--- Example/Pods/Target Support Files/Pods-flutter_sound_core_Tests/Pods-flutter_sound_core_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flutter_sound_core_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_flutter_sound_core_Tests
-@end
--- Example/Pods/Target Support Files/flutter_sound_core/flutter_sound_core-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flutter_sound_core : NSObject
-@end
-@implementation PodsDummy_flutter_sound_core
-@end
