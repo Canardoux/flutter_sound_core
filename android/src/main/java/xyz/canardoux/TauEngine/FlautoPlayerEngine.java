3a156fe6900a50bcb1aac2f5c4fbe6047a2620f5
victor <changcw83@gmail.com>
2024-08-18 22:47:47 +0800

    fix setvolumepan in FlautoPlayerEngine.java

--- android/src/main/java/xyz/canardoux/TauEngine/FlautoPlayerEngine.java
+++ android/src/main/java/xyz/canardoux/TauEngine/FlautoPlayerEngine.java
@@ -179,16 +179,16 @@ class FlautoPlayerEngine extends FlautoPlayerEngineInterface {
 
         if (pan < 0.0f) {
             // Panning to the left
-            leftVolume = 1.0f;
-            rightVolume = 1.0f + (float)pan;  // pan is negative, so this reduces the right volume
+            leftVolume = volume * 1.0f;
+            rightVolume = volume * (1.0f + (float)pan);  // pan is negative, so this reduces the right volume
         } else if (pan > 0.0f) {
             // Panning to the right
-            leftVolume = 1.0f - (float)pan;  // pan is positive, so this reduces the left volume
-            rightVolume = 1.0f;
+            leftVolume = volume * (1.0f - (float)pan);  // pan is positive, so this reduces the left volume
+            rightVolume = volume * 1.0f;
         } else {
             // Center
-            leftVolume = 1.0f;
-            rightVolume = 1.0f;
+            leftVolume = volume * 1.0f;
+            rightVolume = volume * 1.0f;
         }
 
         // Set the stereo volume
